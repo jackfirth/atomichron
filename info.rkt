02eb868ed4d567342b2655bdae12e9e2cdff6350
Jack Firth <jackhfirth@gmail.com>
2019-12-11 20:46:32 -0800

    Fill in repository template parameters

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "atomichron")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "atomichron")))
 
 (define deps
   (list "base"))
